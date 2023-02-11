f071e8d812bc686fc7580c62f8a676e9257d433c
Logan Lentz <logan.lentz1@gmail.com>
2023-02-11 05:00:09 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,12 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+const express = require('express');
+const app = express();
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-}
+app.get('/:hash', (req, res) => {
+  const hash = req.params.hash;
+  const redirectUrl = `https://${hash}.ipfs.w3s.link`;
+  res.redirect(redirectUrl);
+});
 
-export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
-};
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
