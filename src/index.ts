2eaf7b78cd902d1cb71060d34ef7a89f9c03f370
Logan Lentz <logan.lentz1@gmail.com>
2023-02-11 20:35:10 -0600

    removed html page- it should be seperate

--- src/index.ts
+++ src/index.ts
@@ -11,28 +11,3 @@ app.get('/:hash', (req, res) => {
 app.listen(3000, () => {
 	console.log('Server listening on port 3000');
 });
-
-
-<html>
-	<head>
-	<title>CID Router < /title>
-		< /head>
-		< body >
-		<h1>CID Router < /h1>
-			< p > Enter a CID: </p>
-				< input id = "cidInput" type = "text" />
-					<button id="submitButton" > Go < /button>
-
-						<script>
-const submitButton = document.getElementById('submitButton');
-const cidInput = document.getElementById('cidInput');
-
-submitButton.addEventListener('click', () => {
-	const cid = cidInput.value;
-	const url = `https://${cid}.ipfs.w3s.link`;
-	window.location.href = url;
-});
-</script>
-	< /body>
-	< /html>
->>>>>>> 0bf90e73251d5f02c925c574a37a9f2282bc43ba
